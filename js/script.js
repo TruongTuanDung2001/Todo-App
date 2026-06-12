538473c4688fbb96ae27fc59ea3d0d53dd1f944c
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-06-12 13:06:40 +0700

    Create file api tutorial, create format date now

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 //
+let today = document.querySelectorAll('.today');
+function formatDate(){
+    const now = new Date();
+    const day = now.getDay();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+    //
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    //
+    const str_day = now.toDateString();
+    // Định dạng thành chuỗi dd/mm/yyyy hh:mm:ss
+    const formattedDateTime = 
+        `${str_day} - ` +
+        // `${day.toString().padStart(2, '0')}/` +
+        // `${month.toString().padStart(2, '0')}/` +
+        // `${year} ` +
+        `${hours.toString().padStart(2, '0')}:` +
+        `${minutes.toString().padStart(2, '0')}:` +
+        `${seconds.toString().padStart(2, '0')}`;
 
+    return formattedDateTime;
+}
+function updateDate(){
+    today.forEach((e) =>{
+        e.textContent = formatDate();
+    });
+}
+setInterval(updateDate, 1000);
 
 //Get name
 let nameUser = localStorage.getItem('currentUser');
@@ -15,3 +44,4 @@ sidebarLogout.addEventListener('click', function(e){
     
     window.location.href = 'login.html';
 });
+

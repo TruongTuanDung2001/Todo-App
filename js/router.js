8522c454ec7e4f713a5ec51c3d80fa719eabe87a
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-06-16 11:39:47 +0700

    Change file html when click diferent page

--- js/router.js
+++ js/router.js
@@ -1,2 +1,27 @@
 //Chuyển nội dung main thành trang save, mà không load dữ liệu
 
+let layoutMain = document.querySelector('.layout__main');
+
+async function renderPage(page){
+    let response = await fetch(`../html/pages/${page}.html`);
+
+    if(response.ok){
+        let html = await response.text();
+        console.log(html);
+    
+        layoutMain.innerHTML = html;
+    }
+    if(page === 'dashboard'){
+        getTasks();
+    }
+    
+}
+
+document.addEventListener('click', function(e){
+    let page = e.target.dataset.page;
+    if(page){
+        renderPage(page);
+    }
+})
+
+renderPage('dashboard');
